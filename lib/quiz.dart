78a194aebc3f5b045b66b60e82f67e952f46c1bb
saran-mani <saranmvdm@gmail.com>
2023-08-23 17:57:46 +0530

    resultscreen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz/data/questions.dart';
 import 'package:quiz/questions_screen.dart';
 import 'package:quiz/start_screen.dart';
+import 'package:quiz/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -27,7 +28,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -39,6 +40,9 @@ class _QuizState extends State<Quiz> {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
       );
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultScreen();
     }
     return MaterialApp(
       home: Scaffold(
---
+++ lib/results_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: Container(
+          margin: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('You answerd X out of Y questions correctly!'),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text('List of answer and questions...'),
+              const SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Restart Quiz!'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
